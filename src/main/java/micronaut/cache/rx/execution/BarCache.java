3c12d1fbb7dfe0a96f7e3107ac87ba4846fd7f78
Jakob Horndahl <jakob.horndahl@mojang.com>
2021-02-23 14:28:34 +0100

    Use java 11 and use correct cache annotation for config

--- src/main/java/micronaut/cache/rx/execution/BarCache.java
+++ src/main/java/micronaut/cache/rx/execution/BarCache.java
@@ -1,14 +1,15 @@
 package micronaut.cache.rx.execution;
 
+import io.micronaut.cache.annotation.CacheConfig;
 import io.micronaut.cache.annotation.Cacheable;
 import io.reactivex.Single;
 import javax.inject.Singleton;
 
 @Singleton
-@Cacheable("bar-cache") // <-- Remove this
+@CacheConfig("bar-cache")
 public class BarCache {
 
-    @Cacheable // <-- and this for expected behaviour
+    @Cacheable // <-- Remove this for expected behaviour
     public Single<Bar> getBar(String id) {
         return Single.fromCallable(() -> {
             System.out.println("2. Get cacheable Bar");
